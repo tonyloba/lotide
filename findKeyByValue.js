64b7740b384ce172c5ece43598647d12569d9d0a
tonyloba <tonyloba@gmail.com>
2021-09-16 05:09:51 +0000

    add func using forIn loop to find the Key

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function (actual, expected) {
+  const errorMsg = '!==';
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} ${errorMsg} ${expected}`);
+  }
+};
+
+const findKeyByValue = function (obj, val) {
+  for (let key in obj) {
+    if (obj[key] == val) {
+      return key;
+    }
+  }
+  return undefined;
+}
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+  horror: "Jaws"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Jaws"), "horror");
