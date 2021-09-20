91182ab3daeb7d575f7f076448662b0257b92494
tonyloba <tonyloba@gmail.com>
2021-09-20 23:02:30 +0000

    modification for headTest.js with Mocha and Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,19 +1,42 @@
-// EXPORT assertEqual :
-const head = require('../head');
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns 'Hello' for '['Hello', 'Lighthouse', 'Labs']' ", () => {
+    assert.strictEqual(head([100]), 100); 
+  });
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined); 
+  });
+  it("returns '100' for [100]", () => {
+    assert.strictEqual(head([100]), 100); 
+  });
+});
 
 
-const headTest = function (actual, expected) {
-  const errorMsg = '!==';
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} ${errorMsg} ${expected}`);
-  }
 
-};
+// // EXPORT assertEqual :
+// const head = require('../head');
 
-// TEST CODE
-headTest(head([5, 6, 7]), 5);
-headTest(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-headTest(head([]), undefined);
-headTest(head([100]), 100);
+
+// const headTest = function (actual, expected) {
+//   const errorMsg = '!==';
+//   if (actual === expected) {
+//     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`❌ Assertion Failed: ${actual} ${errorMsg} ${expected}`);
+//   }
+
+// };
+
+// // TEST CODE
+// headTest(head([5, 6, 7]), 5);
+// headTest(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// headTest(head([]), undefined);
+// headTest(head([100]), 100);
