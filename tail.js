17aabe0c563ff382a6106085a9a2a6cf9ce470d4
tonyloba <tonyloba@gmail.com>
2021-09-20 19:49:47 +0000

    creat export for tail test

--- tail.js
+++ tail.js
@@ -1,21 +1,8 @@
-const errorMsg = '!==';
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} ${errorMsg} ${expected}`);
-  }
-
-};
 
 const tail = function(arr) {
   return arr.slice(1);
 };
 
-// TEST CODE
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+module.exports = tail;
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+
