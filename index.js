46e58fdfe0cf5c611150660c027fcac9dbe8ffe5
tonyloba <tonyloba@gmail.com>
2021-09-21 00:41:15 +0000

    add shorthand for object properties

--- index.js
+++ index.js
@@ -17,19 +17,19 @@ const without = require('./without');
 
 
 module.exports = {
-  head:head,
-  tail:tail,
-  middle:middle,
-  assertArraysEqual:assertArraysEqual,
-  assertEqual:assertEqual,
-  countLetters:countLetters,
-  countOnly:countOnly,
-  eqArrays:eqArrays,
-  eqObjects:eqObjects,
-  findKey:findKey,
-  findKeyByValue:findKeyByValue,
-  letterPositions:letterPositions,
-  map:map,
-  takeUntil:takeUntil,
-  without:without
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  without
 };
